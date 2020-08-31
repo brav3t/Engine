66609869e810460960415c82c588ebffd0384771
brav3t <brav3t@outlook.com>
2020-08-31 21:26:48 +0200

    Entry Point

---
+++ Engine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Engine application
+
+#include "Engine/Application.h"
+
+// --- Entry Point ---
+#include "Engine/EntryPoint.h"
+// 
---
+++ Engine/src/Engine/Application.cpp
@@ -0,0 +1,18 @@
+#include "Application.h"
+
+namespace Engine {
+
+	Engine::Application::Application()
+	{
+	}
+
+	Engine::Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		while (true);
+	}
+
+}
